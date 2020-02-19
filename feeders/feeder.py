da8eaa32c78690a9e15eebf5c860cee62182379a
Bhupesh-V <varshneybhupesh@gmail.com>
2020-02-19 20:19:26 +0530

    ton of new feeders

--- feeders/feeder.py
+++ feeders/feeder.py
@@ -23,25 +23,51 @@ podcasts = {
     "se-radio": "http://feeds.feedburner.com/se-radio",
     "thechangelog": "https://changelog.com/podcast/feed",
     "fullstackradio": "https://rss.simplecast.com/podcasts/279/rss",
-    "codenewbie": "http://feeds.codenewbie.org/cnpodcast.xml"
+    "codenewbie": "http://feeds.codenewbie.org/cnpodcast.xml",
+    "codingblocks": "https://www.codingblocks.net/feed/podcast/",
+    "coderradio": "https://feeds.fireside.fm/coder/rss",
+    "testandcode": "https://feeds.fireside.fm/testandcode/rss",
+    "stackoverflowpodcast": "https://feeds.simplecast.com/XA_851k3",
+    "weeklydevtips": "https://feeds.simplecast.com/W8bGHhCA"
 }
 
 newsletters = {
     "golangweekly": "https://golangweekly.com/rss",
     "rubyweekly": "https://rubyweekly.com/rss",
     "nodeweekly": "https://nodeweekly.com/rss",
-    "cssweekly": "http://feeds.feedburner.com/CSS-Weekly?format=xml",
+    "cssweekly": "https://feeds.feedburner.com/CSS-Weekly",
     "dbweekly": "https://dbweekly.com/rss",
     "mobiledevweekly": "https://mobiledevweekly.com/rss",
     "javascriptweekly": "https://javascriptweekly.com/rss",
     "frontendfocus": "https://frontendfoc.us/rss",
     "androidweekly": "http://us2.campaign-archive1.com/feed?u=887caf4f48db76fd91e20a06d&id=4eb677ad19",
-    "pycoder": "https://pycoders.com/feed"
-    # add all feeds from libhunt.com
+    "pycoder": "https://pycoders.com/feed",
+    "awesomeandroid": "https://android.libhunt.com/newsletter/feed",
+    "awesomecpp": "https://cpp.libhunt.com/newsletter/feed",
+    "awesomego": "https://go.libhunt.com/newsletter/feed",
+    "awesomejs": "https://js.libhunt.com/newsletter/feed",
+    "awesomepython": "https://python.libhunt.com/newsletter/feed",
+    "awesomedotnet": "https://dotnet.libhunt.com/newsletter/feed",
+    "awesomephp": "https://php.libhunt.com/newsletter/feed",
+    "awesomerust": "https://rust.libhunt.com/newsletter/feed",
+    "awesomeruby": "https://ruby.libhunt.com/newsletter/feed",
+    "awesomeswift": "https://swift.libhunt.com/newsletter/feed",
+    "awesomescala": "https://scala.libhunt.com/newsletter/feed",
+    "awesomenodejs": "https://nodejs.libhunt.com/newsletter/feed",
+    "awesomekotlin": "https://kotlin.libhunt.com/newsletter/feed",
+    "awesomeelixir": "https://elixir.libhunt.com/newsletter/feed",
+    "awesomeios": "https://ios.libhunt.com/newsletter/feed",
+    "awesomejava": "https://java.libhunt.com/newsletter/feed",
+    "awesomesysadmin": "https://sysadmin.libhunt.com/newsletter/feed",
 }
 
 
 def get_feed(url):
+    site_feed = feedparser.parse(url)
+    return site_feed.entries
+
+
+def get_latest_feed(url):
     site_feed = feedparser.parse(url)
     return site_feed.entries[0]
 
@@ -54,34 +80,14 @@ def get_domain(link):
 
 def podcasts_feeds():
     with ThreadPoolExecutor(max_workers=20) as executor:
-        results = list(executor.map(get_feed, podcasts.values()))
-
-    # feed_result = [i for i in itertools.chain.from_iterable(results)]
-
-    # random.shuffle(feed_result)
-
-    # for f in feed_result:
-    #     f["feeder_site"] = get_domain(f["link"])
-
-    print(len(results))
-    print(results[0])
+        results = list(executor.map(get_latest_feed, podcasts.values()))
 
     return results
 
 
 def newsletters_feeds():
     with ThreadPoolExecutor(max_workers=20) as executor:
-        results = list(executor.map(get_feed, newsletters.values()))
-
-    # feed_result = [i for i in itertools.chain.from_iterable(results)]
-
-    # random.shuffle(feed_result)
-
-    # for f in feed_result:
-    #     f["feeder_site"] = get_domain(f["link"])
-
-    print(len(results))
-    print(results[0])
+        results = list(executor.map(get_latest_feed, newsletters.values()))
 
     return results
 
--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-
 function storycount(argument) {
     feed_cards = document.getElementsByClassName("feed-card");
     var ff = document.getElementById("filter-feed");
@@ -16,7 +15,15 @@ function rewrite_summary(argument) {
     }
 }
 
+function rewrite_description(argument) {
+    fs = document.getElementsByClassName("feed-description");
+    for (var i = 0; i < fs.length; i++) {
+        fs[i].innerHTML = fs[i].textContent;
+    }
+}
+
 function append_feed(data) {
+    deleteLoadSVG();
     var mainContainer = document.getElementById("feed");
 
     for (var i = 0; i < data.length; i++) {
@@ -25,8 +32,8 @@ function append_feed(data) {
         feed_card.setAttribute("class", "feed-card");
         h4 = document.createElement("h4");
         h4.textContent = data[i].title;
-/*        blockquote = document.createElement("blockquote");
-        blockquote.textContent = data[i].published_parsed.tm_day;*/
+        /*        blockquote = document.createElement("blockquote");
+                blockquote.textContent = data[i].published_parsed.tm_day;*/
 
         var link = document.createElement("a");
 
@@ -45,6 +52,7 @@ function append_feed(data) {
 
 function fetch_feed(argument) {
     var feed = document.getElementById("feed-input").value;
+    addLoadSVG();
     clear_previous_feed();
 
     get_feed(feed.toLowerCase());
