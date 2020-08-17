6c1d51d32c1fedbeb7871feaa69ba07816334f94
Liam Masters <liam.masters3@gmail.com>
2020-08-17 12:58:18 -0400

    Separate set feed function

--- defe/__main__.py
+++ defe/__main__.py
@@ -100,36 +100,50 @@ def main():
         sys.exit(1)
 
     args = parser.parse_args()
+    
+    set_feed(args.feed, args.max_feed_count, False)
 
-    if args.feed == "general":
+def set_feed(feed_arg, max_feed_count, refresh):
+    if feed_arg == "general":
         data = feedcore.all_feed()
-        for item in data[: args.max_feed_count]:
+        for item in data[: max_feed_count]:
             print(Fore.RED + Style.BRIGHT + str(data.index(item) + 1), end=". ")
             defy(item["feed_src"], item["title"], item["link"])
-        defy_prompt(data)
-
-    if args.feed == "news":
+        if not refresh:
+            defy_prompt(data, feed_arg, max_feed_count)
+        else:
+            return data
+    if feed_arg == "news":
         data = feedcore.news_feed()
-        for item in data[: args.max_feed_count]:
+        for item in data[: max_feed_count]:
             print(Fore.RED + Style.BRIGHT + str(data.index(item) + 1), end=". ")
             defy(item["feed_src"], item["title"], item["link"])
-        defy_prompt(data)
-    if args.feed == "newsletters":
+        if not refresh:
+            defy_prompt(data, feed_arg, max_feed_count)
+        else:
+            return data
+    if feed_arg == "newsletters":
         data = feedcore.newsletters_feeds()
-        for item in data[: args.max_feed_count]:
+        for item in data[: max_feed_count]:
             print(Fore.RED + Style.BRIGHT + str(data.index(item) + 1), end=". ")
             defy(item["feed_src"], item["title"], item["link"])
-        defy_prompt(data)
-    if args.feed == "podcasts":
+        if not refresh:
+            defy_prompt(data, feed_arg, max_feed_count)
+        else:
+            return data
+    if feed_arg == "podcasts":
         data = feedcore.podcasts_feeds()
-        for item in data[: args.max_feed_count]:
+        for item in data[: max_feed_count]:
             print(Fore.RED + Style.BRIGHT + str(data.index(item) + 1), end=". ")
             if item:
                 defy(item["feed_src"], item["title"], item["link"])
             else:
                 pass
-        defy_prompt(data)
-    if args.feed == "feeders":
+        if not refresh:
+            defy_prompt(data, feed_arg, max_feed_count)
+        else:
+            return data
+    if feed_arg == "feeders":
         feeds = ["general", "news", "podcasts", "newsletters"]
         print(
             Style.BRIGHT + "\ndefe fetches feeds of these sources 😃", end="\n\n",
